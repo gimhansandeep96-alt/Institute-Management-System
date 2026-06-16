f2231cb2489a684b670edf6c708a774c4c255eac
gimhan <gimhansandeep96@gmail.com>
2026-06-16 11:24:07 +0530

    Initial commit

---
+++ lk.ijse.institute1/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module lk.ijse.lk.ijse.institute1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens lk.ijse.lk.ijse.institute1 to javafx.fxml;
+    exports lk.ijse.lk.ijse.institute1;
+}
